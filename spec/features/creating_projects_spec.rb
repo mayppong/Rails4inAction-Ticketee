cdb3578076fa024fc4933e5d7b7c80f0dca16ef8
May P <mayppong@gmail.com>
2013-12-01 16:35:38 -0800

    Add name validation
    Create a new test scenario for when creating a new project without name.
    Add validation to ensure names are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario "can create a project" do
-    visit '/'
-    
-    click_link 'New Project'
 
+  before do
+    visit '/'
+    click_link 'New Project'
+  end
+
+  scenario "can create a project" do
     fill_in 'Name', with: 'TextMate 2'
     fill_in 'Description', with: 'A text-editor for OS X'
     click_button 'Create Project'
-
     expect( page ).to have_content( 'Project has been created' )
 
     project = Project.where( name: "TextMate 2" ).first
@@ -17,5 +18,11 @@ feature 'Creating Projects' do
 
     title = "TextMate 2 - Projects - Ticketee"
     expect( page ).to have_title( title )
+  end
+
+  scenario  "can not create a project without a name" do 
+    click_button 'Create Project'
+    expect( page ).to have_content( "Project has not been created" )
+    expect( page ).to have_content( "Name can't be blank" )
   end
 end
