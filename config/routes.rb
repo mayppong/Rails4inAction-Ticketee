574fb373f2373fd1e04a35e8c946abc5700944b0
May P <mayppong@gmail.com>
2013-12-15 18:26:07 -0800

    Implement creating tickets for a project
    Generate a new ticket controller and model.
    Write test cases for ticket creation.
    Implement basic validation for ticket model.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Ticketee::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
 
   # Example resource route with options:
   #   resources :products do
