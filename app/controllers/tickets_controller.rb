df0b396087db59045c9c32f969c56fd83a72b9f3
May P <mayppong@gmail.com>
2013-12-16 21:47:32 -0800

    Implement deleting tickets feature
    Create test case for deleting
    Add a delete link to the ticket's view page and implement destroy method
    in ticket controller.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -28,6 +28,11 @@ class TicketsController < ApplicationController
       render action: "edit"
     end
   end
+  def destroy
+    @ticket.destroy
+    flash[:notice] = 'Ticket has been deleted.'
+    redirect_to @project
+  end
 
   private
   def set_project
