7c3652af7c0734043e890b445171791c109195d5
May P <mayppong@gmail.com>
2014-01-03 20:19:52 -0800

    Added admin flag to user.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
+
   factory :user do
     name     'steven'
     email    'sample@example.com'
     password 'password'
     password_confirmation 'password'
+
+    factory :admin_user do
+      admin true
+    end
+
   end
 end
