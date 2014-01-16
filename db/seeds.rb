12e45958d4759f104271e590b6ffce9dfc541faa
May P <mayppong@gmail.com>
2014-01-15 20:58:38 -0800

    Add a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  admin_user = User.create(
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    admin: true
+  )
+
+  Project.create( name: 'Ticketee Beta' )
---
+++ spec/features/seeds_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Seed Data' do
+  
+  scenario 'the basics' do
+    load Rails.root + "db/seeds.rb"
+    user = User.where( email: 'admin@example.com' ).first!
+    project = Project.where( name: 'Ticketee Beta' ).first!
+  end
+
+end
