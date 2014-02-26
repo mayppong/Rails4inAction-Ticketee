e22fd9a7cdb39562b338c6cedbf1c85e495f13cf
May P <mayppong@gmail.com>
2014-02-25 18:40:11 -0800

    Add search tickets by state
    Users may now search for tickets by state or tags.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,8 @@
 class Ticket < ActiveRecord::Base
 
   searcher do
-    label :tag, :from => :tags, :field => :name
+    label :tag, from: :tags, field: :name
+    label :state, from: :state, field: :name
   end
 
   belongs_to :project
--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -4,19 +4,23 @@ feature 'Searching' do
   let!( :user ) { FactoryGirl.create( :user ) }
   let!( :project ) { FactoryGirl.create( :project ) }
   let!( :ticket_1 ) do
+    state = State.create( name: "Open" )
     FactoryGirl.create( :ticket,
       title: 'Create projects',
       project: project,
       user: user,
-      tag_names: 'iteration_1'
+      tag_names: 'iteration_1',
+      state: state
     )
   end
   let!( :ticket_2 ) do
+    state = State.create( name: "Closed" )
     FactoryGirl.create( :ticket,
       title: 'Create users',
       project: project,
       user: user,
-      tag_names: 'iteration_2'
+      tag_names: 'iteration_2',
+      state: state
     )
   end
   
@@ -38,4 +42,12 @@ feature 'Searching' do
     end
   end
 
+  scenario "finding be state" do
+    fill_in "Search", with: "state:Open"
+    click_button "Search"
+    within( "#tickets" ) do
+      page.should have_content( "Create projects" )
+      page.should_not have_content( "Create users" )
+    end
+  end
 end
